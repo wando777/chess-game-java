84d9978d7cfd5626d0bbd5faad1a99dfecc76900
Wanderson Aragão <wanderson.hal@gmail.com>
2020-01-21 15:03:27 -0300

    Using color when priting the board

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -36,13 +36,27 @@ public class ChessMatch {
 
 	private void initialSetup() {
 		// White pieces
-		placeNewPiece(A, 1, new Rook(board, Color.WHITE));
-		placeNewPiece(H, 1, new Rook(board, Color.WHITE));
-		placeNewPiece(E, 1, new King(board, Color.WHITE));
+//		placeNewPiece(A, 1, new Rook(board, Color.WHITE));
+//		placeNewPiece(H, 1, new Rook(board, Color.WHITE));
+//		placeNewPiece(E, 1, new King(board, Color.WHITE));
+//
+//		placeNewPiece(A, 8, new Rook(board, Color.BLACK));
+//		placeNewPiece(H, 8, new Rook(board, Color.BLACK));
+//		placeNewPiece(D, 8, new King(board, Color.BLACK));
+		placeNewPiece('c', 1, new Rook(board, Color.WHITE));
+		placeNewPiece('c', 2, new Rook(board, Color.WHITE));
+		placeNewPiece('d', 2, new Rook(board, Color.WHITE));
+		placeNewPiece('e', 2, new Rook(board, Color.WHITE));
+		placeNewPiece('e', 1, new Rook(board, Color.WHITE));
+		placeNewPiece('d', 1, new King(board, Color.WHITE));
 
-		placeNewPiece(A, 8, new Rook(board, Color.BLACK));
-		placeNewPiece(H, 8, new Rook(board, Color.BLACK));
-		placeNewPiece(D, 8, new King(board, Color.BLACK));
+		placeNewPiece('c', 7, new Rook(board, Color.BLACK));
+		placeNewPiece('c', 8, new Rook(board, Color.BLACK));
+		placeNewPiece('d', 7, new Rook(board, Color.BLACK));
+		placeNewPiece('e', 7, new Rook(board, Color.BLACK));
+		placeNewPiece('e', 8, new Rook(board, Color.BLACK));
+		placeNewPiece('d', 8, new King(board, Color.BLACK));
+
 	}
 
 }
